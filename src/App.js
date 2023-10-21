ce5ccda04a7829f47a8551f08f600b15c84d3e7e
Sushrut <sushrutathawale1509@gmail.com>
2023-10-21 17:39:48 +0530

    deleting extra items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const Navbar = lazy(() => import("./components/Navbar"));
+const Github = lazy(() => import("./components/Github"));
+const Editor = lazy(() => import("./pages/Editor"));
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = (checked) => {
+    setDarkMode(checked);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' id={`${darkMode ? `dark` : `light`}-mode`}>
+      <Suspense
+        fallback={
+          <div>Loading ... </div>
+          // <Loader />
+        }
+      >
+        <BrowserRouter>
+          <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path='/editor' exact element={<Editor />} />
+            <Route path='/github' exact element={<Github />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
