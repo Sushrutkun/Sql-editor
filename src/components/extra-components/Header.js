2202ddb941bb88b0aeebb8e2bae6c9fbae2adc16
Sushrut <sushrutathawale1509@gmail.com>
2023-10-23 11:42:26 +0530

    creating sidebar editpanel and output

--- src/components/extra-components/Header.js
+++ src/components/extra-components/Header.js
@@ -1,3 +1,20 @@
+// import { Link } from "react-router-dom";
+// import logo from "../../assets/logo.svg";
+
+// const Header = () => {
+//   return (
+//     <div className='header'>
+//       <div className='logo'>
+//         <Link to='/'>
+//           img
+//           {/* <img src={logo} width='100px' height='100%' alt='atlan logo' /> */}
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
 import React from 'react'
 
 const Header = () => {
